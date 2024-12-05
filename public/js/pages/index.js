9d4f8ec73e4f77579a98bbd2026fa07cb7a5f079
Dane Buchanan <daneb@broadbandtap.co.uk>
2024-12-05 15:18:12 +0000

    Hid left column on homepage + added toggle button for left column widgets

--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(updateTweet, 300000);
     setupChat();
     openBtnHotlink();
+    openAdditionalMobile();
 });
 
 async function loadPosts(page = 1, limit = 4) {
@@ -393,4 +394,15 @@ function openBtnHotlink() {
     hotlinkText.addEventListener('click', () => {
         dropdown.classList.toggle('active');
     });
+}
+
+function openAdditionalMobile() {
+    const toggle = document.querySelector('.left-column-toggle');
+    const leftColumn = document.querySelector('.left-column');
+
+  toggle.addEventListener('click', () => {
+    leftColumn.classList.toggle('show');
+    toggle.classList.toggle('active');
+    toggle.textContent = leftColumn.classList.contains('show') ? 'Hide Widgets' : 'Show Widgets';
+  });
 }
